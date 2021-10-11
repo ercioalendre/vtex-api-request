ddec4fa7d701df76341f9e27e08755de2204123e
Ercio Alendre <ercio.alendre@gmail.com>
2021-10-11 00:49:17 -0300

    service to generate reports updated

--- src/services/GenerateReport.service.ts
+++ src/services/GenerateReport.service.ts
@@ -2,7 +2,7 @@ import CreateQueue from "@providers/Queue.provider";
 
 class GenerateReportService {
   public async execute(email: string) {
-    await CreateQueue.execute("GenerateReport", { email });
+    new CreateQueue("GenerateReport", { email });
 
     console.log(`Enqueued a report request to be sent to ${email}`);
   }
